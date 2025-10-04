c0946355f7b7063d9d4eacba74c83dd9309e181d
Eliatosha Lyimo <157199664+Es-Lim-Diety@users.noreply.github.com>
2025-10-04 16:16:57 +0200

    with comments

--- classes.py
+++ classes.py
@@ -20,27 +20,27 @@ class particle:
     
     """integrate velocity to calculate position"""
     def update_position(self,width,height,sidelength,gridheight,gridlist,dt=0.1):
+        # remove particle from its inital grid container
         i = self.hash_grid(sidelength, gridheight)
         if self in gridlist[i].container:        
-            gridlist[i].container.remove(self)
-        
+            gridlist[i].container.remove(self)       
 
         # simple Euler integration
         self.position += self.velocity * dt
+
         # keep the particle inside the screen
         self.wall_sep(width,height)
+
+        # hash program into the correct grid
         i = self.hash_grid( sidelength, gridheight)
         gridlist[i].container.add(self)
 
-    def hash_grid(self, side_length, grid_height):
-        # x coordinate on grid
-        x = self.position[0] // side_length
+    # hashing particles into grid nodes by position
+    def hash_grid(self, side_length, grid_width):
+        coords = self.position // side_length
+        return (int(coords[1] * grid_width + coords[0]))
 
-        # y coordinates on grid
-        y = self.position[1] // side_length
-
-        return (int(y * grid_height + x))
-
+    # separate the particle from the wall
     def wall_sep(self, width, height):
         flag=False
         if self.position[0] > width:
@@ -57,27 +57,22 @@ class particle:
             self.position[1] = 0 + self.radius
         return flag
 
-    def wall_collision(self,width,height,sidelength,gridheight,gridlist):
-        # check collision with vertical wall
+    def wall_collision(self,width,height):
+        # compute box center
         middle=[width/2, height/2]
+
+        # check and resolve collision with vertical wall
         distance_from_middle_x = abs(self.position[0] - middle[0])
         allowed_distance_from_middle_x = middle[0] - self.radius
-
-        # resolve collision
         if distance_from_middle_x >= allowed_distance_from_middle_x:
-            norm_axis = np.array([1,0])
+            self.wall_sep(width, height)
+            norm_axis = np.array([1, 0])
             self.velocity -= 2* np.dot(self.velocity, norm_axis) * norm_axis
         
-        # check collision with horizontal wall
+        # check and resolve collision with horizontal wall
         distance_from_middle_y = abs(self.position[1] - middle[1])
         allowed_distance_from_middle_y = middle[1] - self.radius
-        # resolve collision
         if distance_from_middle_y >= allowed_distance_from_middle_y:
+             self.wall_sep(width, height)
              norm_axis = np.array([0, 1])
-             self.velocity -= 2* np.dot(self.velocity, norm_axis) * norm_axis
-        i = self.hash_grid(sidelength, gridheight)
-        if self in gridlist[i].container:
-            gridlist[i].container.remove(self)
-        if self.wall_sep(width,height):
-            i = self.hash_grid(sidelength, gridheight)
-        gridlist[i].container.add(self)
+             self.velocity -= 2* np.dot(self.velocity, norm_axis) * norm_axis       
